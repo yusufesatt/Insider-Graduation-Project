210a13c0a92f7ec2f2a1cf22c428209bae31b067
Yusuf Esat <xyusufesat@gmail.com>
2023-05-16 01:24:13 +0300

    Row coloring

--- main.js
+++ main.js
@@ -11,37 +11,48 @@ for(let i = 0; i < allDivs.length; i++) {
   });
 }
 
+var row = document.getElementById("row1");
+var divs = row.getElementsByClassName('col');
 
-document.getElementById("col1Green").addEventListener("click", function() {
+for(var i = 0; i < divs.length; i++) {
+  divs[i].addEventListener("click", function() {
     this.style.backgroundColor = "#0dc75b";
   });
-  
+}
 
-  document.getElementById("col2Red").addEventListener("click", function() {
+
+
+
+document.getElementById("row1Green").addEventListener("click", function() {
+    this.style.backgroundColor = "#0dc75b";
+  });
+
+  
+  document.getElementById("row2Red").addEventListener("click", function() {
     this.style.backgroundColor = "#ff054a";
   });
   
-  document.getElementById("col3Orange").addEventListener("click", function() {
+  document.getElementById("row3Orange").addEventListener("click", function() {
     this.style.backgroundColor = "#ff7317";
   });
 
-  document.getElementById("col4Yellow").addEventListener("click", function() {
+  document.getElementById("row4Yellow").addEventListener("click", function() {
     this.style.backgroundColor = "#f5b916";
   });
 
-  document.getElementById("col5Blue").addEventListener("click", function() {
+  document.getElementById("row5Blue").addEventListener("click", function() {
     this.style.backgroundColor = "#1acbf7";
   });
 
-  document.getElementById("col6Purple").addEventListener("click", function() {
+  document.getElementById("row6Purple").addEventListener("click", function() {
     this.style.backgroundColor = "#8056f5";
   });
 
-  document.getElementById("col7Pink").addEventListener("click", function() {
+  document.getElementById("row7Pink").addEventListener("click", function() {
     this.style.backgroundColor = "#ff74b6";
   });
   
-  document.getElementById("col8Raspberry").addEventListener("click", function() {
+  document.getElementById("row8Raspberry").addEventListener("click", function() {
     this.style.backgroundColor = "#c6194d";
   });
 
